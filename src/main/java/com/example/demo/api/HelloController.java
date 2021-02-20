527781b97ad69501944e542851270f1dd17781ec
AgneseAndersone <Agnese.Gabriela.Andersone@gmail.com>
2021-02-20 12:49:34 +0200

    pabeigts utilconfig un rest controllers, pielikts web pie dependencies

--- src/main/java/com/example/demo/api/HelloController.java
+++ src/main/java/com/example/demo/api/HelloController.java
@@ -1,7 +1,13 @@
 package com.example.demo.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @GetMapping ("/name")
+    public String getName() {
+        return "Agnese";
+    }
 }
